1937dae2ca6aa838defdc289d248223f3b7fc431
DeepRolling <18811629018@163.com>
2017-07-20 16:58:41 +0800

    增加了推送功能

--- app/src/main/java/com/jiyun/pandatv/Application/App.java
+++ app/src/main/java/com/jiyun/pandatv/Application/App.java
@@ -2,10 +2,18 @@ package com.jiyun.pandatv.Application;
 
 
 import android.app.Application;
+import android.app.Notification;
 
+import com.jiyun.pandatv.R;
 import com.jiyun.pandatv.base.BaseActivity;
 import com.umeng.socialize.PlatformConfig;
 import com.umeng.socialize.UMShareAPI;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import cn.jpush.android.api.BasicPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
 
 public class App extends Application {
 
@@ -22,6 +30,24 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         UMShareAPI.get(this);
+        JPushInterface.setDebugMode(true);//开启极光推送的debug模式
+        JPushInterface.init(this);//初始化极光推送
+        Set<Integer> days = new HashSet<Integer>();
+        days.add(1);
+        days.add(2);
+        days.add(3);
+        days.add(4);
+        days.add(5);
+        JPushInterface.setPushTime(this,days,8,22);//设置极光推送的接收时间
+        JPushInterface.setSilenceTime(getApplicationContext(), 22, 30, 8, 30);//设置静音时间
+        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(this);
+        builder.statusBarDrawable = R.drawable.logo_ipnda;
+        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL
+                | Notification.FLAG_SHOW_LIGHTS;  //设置为自动消失和呼吸灯闪烁
+        builder.notificationDefaults = Notification.DEFAULT_SOUND
+                | Notification.DEFAULT_VIBRATE
+                | Notification.DEFAULT_LIGHTS;  // 设置为铃声、震动、呼吸灯闪烁都要
+        JPushInterface.setPushNotificationBuilder(1, builder);//设置消息样式
     }
 
 }

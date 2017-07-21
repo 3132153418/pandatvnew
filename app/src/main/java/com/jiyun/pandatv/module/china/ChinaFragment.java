5e26d2cd2ab11f0602eddcadb0f394669ab703b1
3132153418 <3132153418@qq.com>
2017-07-21 14:07:00 +0800

    bbb

--- app/src/main/java/com/jiyun/pandatv/module/china/ChinaFragment.java
+++ app/src/main/java/com/jiyun/pandatv/module/china/ChinaFragment.java
@@ -54,9 +54,6 @@ public class ChinaFragment extends BaseFragment implements ChinaContract.View,Vi
         gg_MyLogin_Image = (ImageView) view.findViewById(R.id.gg_MyLogin_Image);
         gg_MyLogin_Image.setOnClickListener(this);
 
-
-
-
     }
 
     @Override
